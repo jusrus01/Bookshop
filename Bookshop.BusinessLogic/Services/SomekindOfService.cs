2deaed0ab58aca1f04b49a0a1505e4c9ef2694ff
DESKTOP-MPAP6AU\jjust <gt>
2022-09-27 22:20:16 +0300

    Added service

---
+++ Bookshop.BusinessLogic/Services/SomekindOfService.cs
@@ -0,0 +1,13 @@
+﻿using Bookshop.Contracts.DataTransferObjects.Demo;
+using Bookshop.Contracts.Services;
+
+namespace Bookshop.BusinessLogic.Services
+{
+    public class SomekindOfService : ISomekindOfService
+    {
+        public void CreateStuff(CreateDemoDto createDemoDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bookshop.Contracts/Services/ISomekindOfService.cs
@@ -0,0 +1,10 @@
+﻿using Bookshop.Contracts.DataTransferObjects.Demo;
+
+namespace Bookshop.Contracts.Services
+{
+    public interface ISomekindOfService // We will use interfaces for dependency injection.
+        // We could not use them, although usually everyone does :)
+    {
+        void CreateStuff(CreateDemoDto createDemoDto);
+    }
+}

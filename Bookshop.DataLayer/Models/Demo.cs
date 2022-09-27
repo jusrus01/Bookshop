4cb58999d04b524be8154e91ea6fd290f4d2ee41
DESKTOP-MPAP6AU\jjust <gt>
2022-09-27 22:28:05 +0300

    Creating a model to use for new table

---
+++ Bookshop.DataLayer/Models/Demo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Bookshop.DataLayer.Models
+{
+    public class Demo
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+
+        // Adding reference to IdentityUser (which is either Administrator, Client or Manager...)
+        public IdentityUser User { get; set; }
+
+        public string UserId { get; set; }
+    }
+}

9bf8dcb352fbabd010cc34e22a3476d423fb0e1a
DESKTOP-MPAP6AU\jjust <gt>
2022-09-27 22:12:05 +0300

    Created demo page

---
+++ Bookshop.WebApp/Pages/Demo/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bookshop.WebApp.Pages.Demo
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

fceb897816ad539a57fab2c3f67181b749011c75
DESKTOP-MPAP6AU\jjust <gt>
2022-09-27 22:57:56 +0300

    Razor page implementation

--- Bookshop.WebApp/Pages/Demo/Index.cshtml.cs
+++ Bookshop.WebApp/Pages/Demo/Index.cshtml.cs
@@ -1,3 +1,7 @@
+using AutoMapper;
+using Bookshop.Contracts.DataTransferObjects.Demo;
+using Bookshop.Contracts.Services;
+using Bookshop.WebApp.ViewModels.Demo;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,34 @@ namespace Bookshop.WebApp.Pages.Demo
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public CreateDemoViewModel CreateInput { get; set; }
+
+        private readonly IMapper _mapper;
+        private readonly ISomekindOfService _somekindOfService;
+
+        public IndexModel(IMapper mapper, ISomekindOfService somekindOfService) // Using dependecy injection again
+        {
+            _mapper = mapper;
+            _somekindOfService = somekindOfService;
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) // Validation attribute check failed
+            {
+                return Page();
+            }
+
+            var fromViewModelToDto = _mapper.Map<CreateDemoDto>(CreateInput);
+
+            await _somekindOfService.CreateStuffAsync(fromViewModelToDto);
+
+            return RedirectToPage("..//Index"); ;
         }
     }
 }

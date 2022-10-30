84cdc36f38f8ba14acedc46009e1658c5c968713
jusrus01 <jjustinas.ruslys@gmail.com>
2022-10-30 17:11:33 +0200

    Added temporary client edit page

---
+++ Bookshop.WebApp/Pages/Client/Edit.cshtml.cs
@@ -0,0 +1,18 @@
+using Bookshop.Contracts.Constants;
+using Bookshop.WebApp.Attributes;
+using Bookshop.WebApp.PageModels;
+
+namespace Bookshop.WebApp.Pages.Client
+{
+    [RolesAuthorize(BookshopRoles.Client)]
+    public class EditModel : BookshopPageModel
+    {
+        public EditModel() : base(null)
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- Bookshop.WebApp/Pages/Client/List.cshtml.cs
+++ Bookshop.WebApp/Pages/Client/List.cshtml.cs
@@ -1,12 +1,15 @@
 using AutoMapper;
+using Bookshop.Contracts.Constants;
 using Bookshop.Contracts.Generics;
 using Bookshop.Contracts.Services;
+using Bookshop.WebApp.Attributes;
 using Bookshop.WebApp.PageModels;
 using Bookshop.WebApp.ViewModels.Clients;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookshop.WebApp.Pages.Client
 {
+    [RolesAuthorize(BookshopRoles.Client)]
     public class ListModel : SinglePaginationBookshopPagedModel<PartialClientViewModel>
     {
         private readonly IClientService _clientService;

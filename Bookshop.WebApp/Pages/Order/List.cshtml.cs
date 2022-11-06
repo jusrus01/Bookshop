4a54e20f59ef9cc194c4ee6d4e548f45027028d3
Greta56 <greta.gabrenaite@gmail.com>
2022-11-06 13:57:00 +0200

    created order navigation and table

--- Bookshop.WebApp/Pages/Order/List.cshtml.cs
+++ Bookshop.WebApp/Pages/Order/List.cshtml.cs
@@ -7,35 +7,11 @@ using Bookshop.WebApp.PageModels;
 using Bookshop.WebApp.ViewModels.Clients;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bookshop.WebApp.Pages.Book
+namespace Bookshop.WebApp.Pages.Order
 {
-    [RolesAuthorize(BookshopRoles.Client)]
-    public class ListModel : SinglePaginationBookshopPagedModel<PartialClientViewModel>
+   
+    public class ListModel
     {
-        private readonly IClientService _clientService;
-        private readonly IMapper _mapper;
-        private readonly IBookService _bookService;
-
-        private const int PageSize = 4;
-
-        public ListModel(IClientService clientService, IMapper mapper, IBookService bookService)
-            :
-            base(null)
-        {
-            _mapper = mapper;
-            _clientService = clientService;
-            _bookService = bookService;
-        }
-
-        public async Task<IActionResult> OnGetAsync(int pageNumber = 1)
-        {
-            CurrentPage = pageNumber;
-
-            var clients = await _clientService.GetClientsPagedAsync(CurrentPage, PageSize);
-
-            Paged = _mapper.Map<Paged<PartialClientViewModel>>(clients);
-
-            return Page();
-        }
+        
     }
 }

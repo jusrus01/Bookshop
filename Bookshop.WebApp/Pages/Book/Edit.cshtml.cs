b9a14db70175ffb733beeea917c989de3d00d211
SimonaRa20 <simonaaragauskaite@gmail.com>
2022-12-03 20:37:53 +0200

    commit changes

--- Bookshop.WebApp/Pages/Book/Edit.cshtml.cs
+++ Bookshop.WebApp/Pages/Book/Edit.cshtml.cs
@@ -1,8 +1,11 @@
+using Bookshop.Contracts;
 using Bookshop.Contracts.Constants;
 using Bookshop.Contracts.Services;
+using Bookshop.DataLayer.Models;
 using Bookshop.WebApp.Attributes;
 using Bookshop.WebApp.PageModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookshop.WebApp.Pages.Book
 {
@@ -10,10 +13,12 @@ namespace Bookshop.WebApp.Pages.Book
     public class EditModel : BookshopPageModel
     {
         private readonly IBookService _bookService;
+        private readonly DbSet<Supplier> _supplierDbSet;
 
-        public EditModel(IBookService bookService) : base(null)
+        public EditModel(IBookService bookService, IUnitOfWork uow) : base(null)
         {
             _bookService = bookService;
+            _supplierDbSet = uow.GetDbSet<Supplier>();
         }
 
         public EditModel() : base(null)
@@ -29,6 +34,14 @@ namespace Bookshop.WebApp.Pages.Book
 
             _bookService.GetBookAsync(id);
             return Page();
+        }
+
+        public List<string> Authors()
+        {
+            List<string> authors = new List<string>();
+            authors.AddRange(_supplierDbSet.GetAsyncEnumerator().Current());
+
+            return authors;
         }
     }
 }

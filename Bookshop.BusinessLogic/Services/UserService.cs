5429820714e517513118bce37b9a4869f3df981a
jusrus01 <jjustinas.ruslys@gmail.com>
2022-12-14 12:59:27 +0200

    Refactored and added logic that updates last login field when logging in

--- Bookshop.BusinessLogic/Services/UserService.cs
+++ Bookshop.BusinessLogic/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using Bookshop.Contracts.Constants;
+﻿using Bookshop.Contracts;
+using Bookshop.Contracts.Constants;
 using Bookshop.Contracts.DataTransferObjects.Users;
 using Bookshop.Contracts.Services;
 using Bookshop.DataLayer.Models;
@@ -31,37 +32,48 @@ namespace Bookshop.BusinessLogic.Services
         public async Task ConfirmEmailAsync(string token, string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
-
-            if (user == null)
-            {
-                throw new Exception("User does not exist");
-            }
-
+            EnsureValidUser(user);
             var result = await _userManager.ConfirmEmailAsync(user, token);
+            EnsureSuccessfulEmailConfirmation(result);
+        }
 
-            if (!result.Succeeded)
-            {
-                throw new Exception("Could not confirm email");
-            }
+        public async Task SignInAsync(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            EnsureValidUser(user);
+            await _signInManager.SignOutAsync();
+            var result = await _signInManager.PasswordSignInAsync(user, loginDto.Password, false, false);
+            EnsureUserConfirmedEmail(result);
+            await UpdateUserLastLoginAsync(user);
         }
 
         public async Task RegisterAsync(RegisterDto registerDto)
+        {
+            await EnsureUserDoesNotExistAsync(registerDto);
+            var newUser = await CreateNewUserAsync(registerDto);
+            await _userManager.AddToRoleAsync(newUser, BookshopRoles.Client);
+            await SendConfirmationEmailAsync(newUser);
+        }
+
+        private async Task EnsureUserDoesNotExistAsync(RegisterDto registerDto)
         {
             if (await IsUserAlreadyRegisteredAsync(registerDto.Email))
             {
                 throw new Exception("User is already registered");
             }
-           
-            var newUser = await CreateNewUserAsync(registerDto);
-
-            await _userManager.AddToRoleAsync(newUser, BookshopRoles.Client);
-
-            await SendConfirmationEmailAsync(newUser);
         }
 
         private async Task<bool> IsUserAlreadyRegisteredAsync(string email)
         {
             return await _userManager.FindByEmailAsync(email) != null;
+        }
+
+        private static void EnsureSuccessfulEmailConfirmation(IdentityResult result)
+        {
+            if (!result.Succeeded)
+            {
+                throw new Exception("Could not confirm email");
+            }
         }
 
         private async Task<ApplicationUser> CreateNewUserAsync(RegisterDto registerDto)
@@ -103,23 +115,26 @@ namespace Bookshop.BusinessLogic.Services
                 confirmationLink));
         }
 
-        public async Task SignInAsync(LoginDto loginDto)
+        private static void EnsureValidUser(ApplicationUser user)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
-
             if (user == null)
             {
                 throw new Exception("User not found");
             }
+        }
 
-            await _signInManager.SignOutAsync();
-
-            var result = await _signInManager.PasswordSignInAsync(user, loginDto.Password, false, false);
-            
+        private static void EnsureUserConfirmedEmail(SignInResult result)
+        {
             if (!result.Succeeded)
             {
                 throw new Exception("Account not confirmed");
             }
+        }
+
+        private async Task UpdateUserLastLoginAsync(ApplicationUser user)
+        {
+            user.LastLogin = DateTime.Now;
+            await _userManager.UpdateAsync(user);
         }
 
         public async Task SignOutAsync()

81bc1a687cb1ac2587e2a1d84bdee8a46d4d4fba
SimonaRa20 <simonaaragauskaite@gmail.com>
2022-12-16 18:47:37 +0200

    added filters

--- Bookshop.Contracts/Services/IBookService.cs
+++ Bookshop.Contracts/Services/IBookService.cs
@@ -7,15 +7,19 @@ namespace Bookshop.Contracts.Services
     {
         Task<BookDto> GetBookAsync(int bookId);
         Task<Paged<PartialBookDto>> GetBooksPagedAsync(int page, int pageSize);
-        Task<GenreDto> GetGenreAsync(int genreId);
-        Task<SupplierDto> GetAuthorAsync(int authorId);
         Task DeleteBookAsync(int? id);
-        Task<BookDto> GetBookISBNAsync(string isbn);
         Task UpdateAsync(BookDto bookDto);
         Task<List<GenreDto>> GetGenres();
         Task<List<SupplierDto>> GetSupplier();
         Task AddAsync(BookDto bookDto);
         Task<List<BookCommentDto>> GetComments(int bookId);
         Task AddComment(BookCommentDto comment);
+        Task<List<string>> GetAuthor();
+        Task<Paged<PartialBookDto>> GetBooksByPricePagedAsync(int page, int pageSize, FilterDto filterDto);
+        Task<Paged<PartialBookDto>> GetBooksByTitlePagedAsync(int page, int pageSize, FilterDto filterDto);
+        Task<Paged<PartialBookDto>> GetBooksByDiscountPagedAsync(int page, int pageSize, FilterDto filterDto);
+        Task<Paged<PartialBookDto>> GetBooksByAuthorPagedAsync(int page, int pageSize, FilterDto filterDto);
+
+
     }
 }
--- Bookshop.WebApp/Pages/Book/List.cshtml.cs
+++ Bookshop.WebApp/Pages/Book/List.cshtml.cs
@@ -1,11 +1,14 @@
 using AutoMapper;
 using Bookshop.Contracts.Constants;
+using Bookshop.Contracts.DataTransferObjects.Books;
 using Bookshop.Contracts.Generics;
 using Bookshop.Contracts.Services;
 using Bookshop.WebApp.Attributes;
 using Bookshop.WebApp.PageModels;
+using Bookshop.WebApp.ViewModels.Books;
 using Bookshop.WebApp.ViewModels.Clients;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bookshop.WebApp.Pages.Book
 {
@@ -14,6 +17,11 @@ namespace Bookshop.WebApp.Pages.Book
     {
         private readonly IBookService _bookService;
         private readonly IMapper _mapper;
+
+        [BindProperty]
+        public FilterViewModel Filter { get; set; }
+
+        public List<SelectListItem> Authors { get; set; }
 
         public ListModel(IBookService bookService, IMapper mapper)
             :
@@ -25,6 +33,14 @@ namespace Bookshop.WebApp.Pages.Book
 
         public async Task<IActionResult> OnGetAsync(int pageNumber = 1)
         {
+            List<string> authors = await _bookService.GetAuthor();
+            List<SelectListItem> authorsList = new List<SelectListItem>();
+            for (int i = 0; i < authors.Count; i++)
+            {
+                authorsList.Add(new SelectListItem { Text = authors[i].ToString(), Value = authors[i].ToString() });
+            }
+            this.Authors = authorsList;
+
             var books = await _bookService.GetBooksPagedAsync(pageNumber, pageSize: 4);
 
             SetPageItems(_mapper.Map<Paged<PartialBookViewModel>>(books), pageNumber);
@@ -32,7 +48,147 @@ namespace Bookshop.WebApp.Pages.Book
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int id)
+        public async Task<IActionResult> OnPostFilterTitleAsync(int pageNumber = 1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+
+                FilterDto filters = new FilterDto
+                {
+                    Title = Filter.Title
+                };
+
+                var books = await _bookService.GetBooksByTitlePagedAsync(pageNumber, pageSize: 10, filters);
+
+                SetPageItems(_mapper.Map<Paged<PartialBookViewModel>>(books), pageNumber);
+
+                List<string> authors = await _bookService.GetAuthor();
+                List<SelectListItem> authorsList = new List<SelectListItem>();
+                for (int i = 0; i < authors.Count; i++)
+                {
+                    authorsList.Add(new SelectListItem { Text = authors[i].ToString(), Value = authors[i].ToString() });
+                }
+                this.Authors = authorsList;
+
+                return Page();
+            }
+            catch (Exception e)
+            {
+                return PageWithError(e.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnPostFilterDiscountAsync(int pageNumber = 1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                FilterDto filters = new FilterDto
+                {
+                    MinDicount = Filter.MinDiscount,
+                    MaxDicount = Filter.MaxDiscount
+                };
+
+                var books = await _bookService.GetBooksByDiscountPagedAsync(pageNumber, pageSize: 10, filters);
+
+                SetPageItems(_mapper.Map<Paged<PartialBookViewModel>>(books), pageNumber);
+
+                List<string> authors = await _bookService.GetAuthor();
+                List<SelectListItem> authorsList = new List<SelectListItem>();
+                for (int i = 0; i < authors.Count; i++)
+                {
+                    authorsList.Add(new SelectListItem { Text = authors[i].ToString(), Value = authors[i].ToString() });
+                }
+                this.Authors = authorsList;
+
+                return Page();
+            }
+            catch (Exception e)
+            {
+                return PageWithError(e.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnPostFilterAuthorAsync(int pageNumber = 1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+
+                FilterDto filters = new FilterDto
+                {
+                    Author = Filter.Author,
+                };
+
+                var books = await _bookService.GetBooksByAuthorPagedAsync(pageNumber, pageSize: 4, filters);
+
+                SetPageItems(_mapper.Map<Paged<PartialBookViewModel>>(books), pageNumber);
+
+                List<string> authors = await _bookService.GetAuthor();
+                List<SelectListItem> authorsList = new List<SelectListItem>();
+                for (int i = 0; i < authors.Count; i++)
+                {
+                    authorsList.Add(new SelectListItem { Text = authors[i].ToString(), Value = authors[i].ToString() });
+                }
+                this.Authors = authorsList;
+
+                return Page();
+            }
+            catch (Exception e)
+            {
+                return PageWithError(e.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnPostFilterPriceAsync(int pageNumber = 1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                FilterDto filters = new FilterDto
+                {
+                    MinPrice = Filter.MinPrice,
+                    MaxPrice = Filter.MaxPrice
+                };
+
+                var books = await _bookService.GetBooksByPricePagedAsync(pageNumber, pageSize: 10, filters);
+
+                SetPageItems(_mapper.Map<Paged<PartialBookViewModel>>(books), pageNumber);
+
+                List<string> authors = await _bookService.GetAuthor();
+                List<SelectListItem> authorsList = new List<SelectListItem>();
+                for (int i = 0; i < authors.Count; i++)
+                {
+                    authorsList.Add(new SelectListItem { Text = authors[i].ToString(), Value = authors[i].ToString() });
+                }
+                this.Authors = authorsList;
+
+                return Page();
+            }
+            catch (Exception e)
+            {
+                return PageWithError(e.Message);
+            }
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
         {
             await _bookService.DeleteBookAsync(id);
             return await OnGetAsync();

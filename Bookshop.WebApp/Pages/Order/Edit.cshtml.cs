d39e6467f7a25a68f8142fab912828d3d9436626
jusrus01 <jjustinas.ruslys@gmail.com>
2022-12-17 18:15:41 +0200

    ....

--- Bookshop.WebApp/Pages/Order/Edit.cshtml.cs
+++ Bookshop.WebApp/Pages/Order/Edit.cshtml.cs
@@ -1,106 +1,30 @@
 using AspNetCoreHero.ToastNotification.Abstractions;
-using AutoMapper;
-using Bookshop.BusinessLogic.Services;
-using Bookshop.Contracts.DataTransferObjects.Books;
+using Bookshop.Contracts.Constants;
 using Bookshop.Contracts.DataTransferObjects.Orders;
-using Bookshop.Contracts.Enums;
 using Bookshop.Contracts.Services;
-using Bookshop.DataLayer.Models;
+using Bookshop.WebApp.Attributes;
 using Bookshop.WebApp.PageModels;
-using Bookshop.WebApp.ViewModels.Books;
-using Bookshop.WebApp.ViewModels.Orders;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Linq;
 
 namespace Bookshop.WebApp.Pages.Orders
 {
+    [AuthorizeAnyOfTheRoles(BookshopRoles.Administrator, BookshopRoles.Manager)]
     public class EditModel : BookshopPageModel
     {
-        [BindProperty]
-        public CreateOrderViewModel OrderInput { get; set; } = new();
-
         private readonly IOrderService _orderService;
-        private readonly IMapper _mapper;
 
-        public List<SelectListItem> Books { get; set; }
-        public List<SelectListItem> Payments { get; set; }
+        [BindProperty]
+        public OrderDto2 Order { get; set; }
 
-        public List<SelectListItem> OrderMethods { get; set; }
-
-        public EditModel(IOrderService orderService, IMapper mapper, INotyfService notyfService) : base(notyfService)
+        public EditModel(IOrderService orderService, INotyfService notyfService) : base(notyfService)
         {
             _orderService = orderService;
-            _mapper = mapper;
         }
 
-        public async Task<IActionResult> OnGetAsync(int id)
+        public async Task<IActionResult> OnGet(int id)
         {
-            //OrderDto order = await _orderService.GetOrderAsync(id);
-            //OrderInput = new CreateOrderViewModel
-            //{
-            //    Id = id,
-            //    Sum = order.Sum,
-            //    PostalCode = order.PostalCode,
-            //    Address = order.Address,
-            //    ClientComment = order.ClientComment,
-            //    OrderMethod = order.OrderMethod,
-            //    PaymentMethod = order.PaymentMethod,
-            //    ExpectedDelivery = order.ExpectedDelivery,
-            //    PaymentDate = order.PaymentDate,
-            //    Status = order.Status,
-            //    UserId = order.UserId,
-            //    Books = order.Books.Select(book => new BookDtoDto { Id = book.Id, Name = book.Name }).ToList()
-            //};
-
-
-            //List<BookDto> books = await _orderService.GetBooks();
-            //List<SelectListItem> bookList = new List<SelectListItem>();
-
-            //List<PaymentMethod> payments = Enum.GetValues(typeof(PaymentMethod)).Cast<PaymentMethod>().ToList();
-            //List<SelectListItem> paymentsList = new List<SelectListItem>();
-
-            //List<OrderMethod> ordersMethods = Enum.GetValues(typeof(OrderMethod)).Cast<OrderMethod>().ToList();
-            //List<SelectListItem> ordersMethodsList = new List<SelectListItem>();
-
-            //for (int i = 0; i < books.Count; i++)
-            //{
-            //    bookList.Add(new SelectListItem { Text = books[i].Title, Value = books[i].Id.ToString() });
-            //}
-
-            //for (int i = 0; i < payments.Count; i++)
-            //{
-            //    paymentsList.Add(new SelectListItem { Text = payments[i].ToString(), Value = payments[i].ToString() });
-            //}
-
-            //for (int i = 0; i < ordersMethods.Count; i++)
-            //{
-            //    ordersMethodsList.Add(new SelectListItem { Text = ordersMethods[i].ToString(), Value = ordersMethods[i].ToString() });
-            //}
-
-            //this.Books = bookList;
-            //this.Payments = paymentsList;
-            //this.OrderMethods = ordersMethodsList;
+            Order = await _orderService.GetOrderAsync(id);
             return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-            try
-            {
-                await _orderService.UpdateAsync(_mapper.Map<OrderDto>(OrderInput));
-            }
-            catch (Exception e)
-            {
-                return PageWithError(e.Message);
-            }
-
-            return RedirectToPage("List");
         }
     }
 }
